0d7b1649d2302c59e49da150566b47500567149d
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-08-10 22:57:14 +0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def factorial(num: int) -> int:
+    return 1 if num == 1 else num * factorial(num - 1)
+
+
 def main():
-    pass
+    result = factorial(9)
+    print(result)
 
 
 if __name__ == '__main__':
