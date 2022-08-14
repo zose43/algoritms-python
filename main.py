b70305e05576881856a9f88d2220068591becb6a
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-08-14 13:58:24 +0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+import array
+
+numbers = [200, 23, 12, 21, 104, 44, 54, 65, 5, 3, 98, 27]
+
+
+def find_bg(data: array) -> int:
+    if len(data) == 2:
+        return data[0] if data[0] > data[1] else data[1]
+
+    sub_max = find_bg(data[1:])
+    return sub_max if data[0] < sub_max else data[0]
+
+
 def main():
-    pass
+    print(find_bg(numbers))
 
 
 if __name__ == '__main__':
